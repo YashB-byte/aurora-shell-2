fcbdbc774212cc5c2d6522835dcb2dcf9372886e
YashB-byte <behera.yash@gmail.com>
2026-01-30 13:22:09 -0800

    Add install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+# Create the directory
+mkdir -p ~/aurora-shell-2
+cd ~/aurora-shell-2
+
+# Initialize git
+git init
+git remote add origin https://github.com/YashB-byte/aurora-shell-2.git
+
+# Create the install script
+nano install.sh
